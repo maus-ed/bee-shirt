d903a80e516fd02ed33cc0de63a8cf8df7c0338e
vuongnq73 <145737558+vuongnq73@users.noreply.github.com>
2024-11-10 17:49:26 +0700

    statics search about day done

--- src/main/java/com/example/bee_shirt/dto/request/BillDTO.java
+++ src/main/java/com/example/bee_shirt/dto/request/BillDTO.java
@@ -8,7 +8,6 @@ import java.time.LocalDate;
 @Data
 public class BillDTO {
 
-
     private String codeBill;
     private String customerName;
     private LocalDate desiredDate;
@@ -16,8 +15,9 @@ public class BillDTO {
     private BigDecimal totalMoney;
     private Integer statusBill;
 
-    // Constructor, getters và setters
-    public BillDTO(String codeBill,String customerName, LocalDate desiredDate,String namePaymentMethod, BigDecimal totalMoney, Integer statusBill) {
+    // Constructor
+    public BillDTO(String codeBill, String customerName, LocalDate desiredDate,
+                   String namePaymentMethod, BigDecimal totalMoney, Integer statusBill) {
         this.codeBill = codeBill;
         this.customerName = customerName;
         this.desiredDate = desiredDate;
@@ -25,5 +25,4 @@ public class BillDTO {
         this.totalMoney = totalMoney;
         this.statusBill = statusBill;
     }
-
 }
